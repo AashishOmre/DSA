64edbd875470ddae1142b0173b80e8658e554f93
Aashu <aashishomre77@gmail.com>
2024-01-16 07:45:33 +0530

    testing

--- 16-JAN-24/kmp.java
+++ 16-JAN-24/kmp.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class kmp {
      static int i=1;
     public static void main(String[] args) {
-        while(i++ >0)
+        while(i++ > 0)
          System.out.println("HELLO JAVA "+ i);
     }
 
