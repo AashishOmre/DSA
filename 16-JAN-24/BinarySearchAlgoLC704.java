50346dd7d65dc9b90baf7f68182d9b558d467e9b
Aashu <aashishomre77@gmail.com>
2024-01-21 17:30:41 +0530

    LC-33

---
+++ 16-JAN-24/BinarySearchAlgoLC704.java
@@ -0,0 +1,34 @@
+public class BinarySearchAlgoLC704 {
+    
+      public static void main(String[] args) {
+
+        Solution sol= new Solution();
+
+        System.out.println(sol.search(new int[]{-1,0,3,5,9,12},2));
+
+      }
+      
+}
+
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int left=0;
+        int right=nums.length-1;
+        int mid=0;
+        while(left<=right)
+        {
+               
+           mid=(left+right)/2;
+           
+           if(nums[mid] < target)
+             left=mid+1;
+            else if(nums[mid] > target)
+             right=mid-1;
+            else
+               return mid; 
+        }
+
+        return -1;
+    }
+}
